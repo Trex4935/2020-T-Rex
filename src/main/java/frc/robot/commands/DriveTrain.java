d27a438bb220e53e2c528983e606ec1a469dc94a
Weltgeist <narcisse118@gmail.com>
2021-01-18 15:02:01 -0500

    changed package to command and chose new commandbase class instead of command interface
    ref:https://docs.wpilib.org/en/stable/docs/software/commandbased/command-based-changes.html

--- src/main/java/frc/robot/commands/DriveTrain.java
+++ src/main/java/frc/robot/commands/DriveTrain.java
@@ -2,11 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class DriveTrain extends Command {
+public class DriveTrain extends CommandBase {
   public DriveTrain() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
