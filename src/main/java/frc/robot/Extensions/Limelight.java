b929c62e2ac87421c1ddd148bece1e44218fb49d
Matt <39496169+Canthv0@users.noreply.github.com>
2021-03-13 16:33:02 -0500

    Changed to statics

--- src/main/java/frc/robot/Extensions/Limelight.java
+++ src/main/java/frc/robot/Extensions/Limelight.java
@@ -12,66 +12,66 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 public class Limelight {
 
 // Reading Limelight values from NetworkTables and creating objects to re-use
-NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-NetworkTableEntry tx = table.getEntry("tx");
-NetworkTableEntry ty = table.getEntry("ty");
-NetworkTableEntry ta = table.getEntry("ta");
-NetworkTableEntry tv = table.getEntry("tv");
-NetworkTableEntry ts = table.getEntry("ts");
-NetworkTableEntry tshort = table.getEntry("tshort");
-NetworkTableEntry tlong = table.getEntry("tlong"); 
-NetworkTableEntry thor = table.getEntry("thor");
-NetworkTableEntry tvert = table.getEntry("tvert");
+static NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+static NetworkTableEntry tx = table.getEntry("tx");
+static NetworkTableEntry ty = table.getEntry("ty");
+static NetworkTableEntry ta = table.getEntry("ta");
+static NetworkTableEntry tv = table.getEntry("tv");
+static NetworkTableEntry ts = table.getEntry("ts");
+static NetworkTableEntry tshort = table.getEntry("tshort");
+static NetworkTableEntry tlong = table.getEntry("tlong"); 
+static NetworkTableEntry thor = table.getEntry("thor");
+static NetworkTableEntry tvert = table.getEntry("tvert");
 
 // Limelight X axis value read into object
-public double getLimeLightX(){
+public static double getLimeLightX(){
 double limeLightX = tx.getDouble(0.0);
 return limeLightX;
 }
 
 // Limelight Y axis value read into object
-public double getLimeLightY(){
+public static double getLimeLightY(){
 double limeLightY = ty.getDouble(0.0);
 return limeLightY;
 }
 
 // Limelight Area value read into object
-public double getLimeLightArea(){
+public static double getLimeLightArea(){
 double limeLightArea = ta.getDouble(0.0);
 return limeLightArea;
 }
 
 // Limelight Valid Target read into object
-public double getLimeLightV(){
+public static double getLimeLightV(){
 double limeLightV = tv.getDouble(0.0);
 return limeLightV; 
 }
 
 // Limelight Skew read into object
-public double getLimeLightS(){
+public static double getLimeLightS(){
 double limeLightS = ts.getDouble(0.0);
 return limeLightS; 
 }
 // Limelight Shortest Sidelength read into object
-public double getLimeLighttshort(){
+public static double getLimeLighttshort(){
 double limeLighttshort = tshort.getDouble(0.0);
 return limeLighttshort;
 }
 
 // Limelight Longest Sidelength read into object
-public double getLimeLighttlong(){
+public static double getLimeLighttlong(){
 double limeLighttlong = tlong.getDouble(0.0);
 return limeLighttlong; 
 }
 
 // Limelight Horizontal Sidelength read into object
-public double getLimeLightthor(){
+public static double getLimeLightthor(){
 double limeLightthor = thor.getDouble(0.0);
 return limeLightthor;
 }
 
 // Limelight Vertical Sidelength read into object
-public double getLimeLighttvert(){
+public static double getLimeLighttvert(){
 double limeLighttvert = tvert.getDouble(0.0);
 return limeLighttvert; 
 }
