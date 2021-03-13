db7904cc3ba10fa3ec06cbb9c2013943b5585563
R Smith <robertcsmith719@gmail.com>
2021-03-13 09:50:18 -0500

    Added new limelight objects

--- src/main/java/frc/robot/Extensions/Limelight.java
+++ src/main/java/frc/robot/Extensions/Limelight.java
@@ -16,6 +16,12 @@ NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
 NetworkTableEntry tx = table.getEntry("tx");
 NetworkTableEntry ty = table.getEntry("ty");
 NetworkTableEntry ta = table.getEntry("ta");
+NetworkTableEntry tv = table.getEntry("tv");
+NetworkTableEntry ts = table.getEntry("ts");
+NetworkTableEntry tshort = table.getEntry("tshort");
+NetworkTableEntry tlong = table.getEntry("tlong"); 
+NetworkTableEntry thor = table.getEntry("thor");
+NetworkTableEntry tvert = table.getEntry("tvert");
 
 // Limelight X axis value read into object
 public double getLimeLightX(){
@@ -35,4 +41,38 @@ double limeLightArea = ta.getDouble(0.0);
 return limeLightArea;
 }
 
+// Limelight Valid Target read into object
+public double getLimeLightV(){
+double limeLightV = tv.getDouble(0.0);
+return limeLightV; 
+}
+
+// Limelight Skew read into object
+public double getLimeLightS(){
+double limeLightS = ts.getDouble(0.0);
+return limeLightS; 
+}
+// Limelight Shortest Sidelength read into object
+public double getLimeLighttshort(){
+double limeLighttshort = tshort.getDouble(0.0);
+return limeLighttshort;
+}
+
+// Limelight Longest Sidelength read into object
+public double getLimeLighttlong(){
+double limeLighttlong = tlong.getDouble(0.0);
+return limeLighttlong; 
+}
+
+// Limelight Horizontal Sidelength read into object
+public double getLimeLightthor(){
+double limeLightthor = thor.getDouble(0.0);
+return limeLightthor;
+}
+
+// Limelight Vertical Sidelength read into object
+public double getLimeLighttvert(){
+double limeLighttvert = tvert.getDouble(0.0);
+return limeLighttvert; 
+}
 }
