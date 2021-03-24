cda96e313d69e4d9c9d289e8c6219b419dacc667
Samuel Narcisse <narcisse118@gmail.com>
2021-03-24 16:17:28 -0400

    added basic copy bath file

---
+++ src/main/java/frc/robot/Extensions/copyPath.bat
@@ -0,0 +1,5 @@
+cd F:\GIT\FIRST\Trex\src\main\java\frc\robot\Extensions
+cp -v ../../../../../../PathWeaver/output/*.wpilib.json ../../../../../../src/main/deploy/paths/
+cp -v ../../../../../../PathWeaver/PathWeaver/output/*.wpilib.json ../../../../../../src/main/deploy/paths/
+cp -v ../../../../../../PathWeaver/PathWeaver/PathWeaver/output/*.wpilib.json ../../../../../../src/main/deploy/paths/
+cp -v ../../../../../../PathWeaver/Paths/PathWeaver/output/*.wpilib.json ../../../../../../src/main/deploy/paths/
