ff7062442cbd59b7851db97c243d867f35728b30
Samuel Narcisse <narcisse118@gmail.com>
2021-03-24 19:59:40 -0400

    added command base group

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Extensions.DriveEncoders;
 import frc.robot.Extensions.Init_Dashboard;
