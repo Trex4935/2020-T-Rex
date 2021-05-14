ad223b1f8d1df9fb412d177b199ecb8f8bf6d0cf
Nugget <matbyrd@forthebyrds.com>
2021-05-14 17:28:18 -0400

    limelight import removal

--- src/main/java/frc/robot/commands/AutoAimCommand.java
+++ src/main/java/frc/robot/commands/AutoAimCommand.java
@@ -5,7 +5,6 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Extensions.Limelight;
 import frc.robot.subsystems.DriveTrain;
 
 public class AutoAimCommand extends CommandBase {
