edb6c881c668a2f2135b3d56cf9b091a6b995ff5
Canthv0 <matbyrd@forthebyrds.com>
2021-06-19 15:31:06 -0400

    PID Tuning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -107,10 +107,10 @@ public final class Constants {
 
     // Pid values kP kI kD kF Iz PeakOut
     // public final static Gains kGains_Velocity_Shooter = new Gains(0.74, 0.001, 5, 1023.0 / 20660.0, 300, 1.00);
-    public final static Gains kGains_Velocity_Shooter = new Gains(0.1, 0.001, 5, 1023.0 / 20660.0, 300, 1.00);
+    public final static Gains kGains_Velocity_Shooter = new Gains(0.6, 0.001, 0, 0.2, 300, 1.00);
 
     // Target RPM value for shooter motor
-    public static final int targetRPM = 5000;
+    public static final int targetRPM = 3000;
 
     // endregion
 
