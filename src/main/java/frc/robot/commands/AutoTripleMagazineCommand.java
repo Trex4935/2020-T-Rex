82033121be6d8474a2adcc0f7a7b3cbc4cfd4eaa
Canthv0 <matbyrd@forthebyrds.com>
2021-07-17 10:50:50 -0400

    cleanup

--- src/main/java/frc/robot/commands/AutoTripleMagazineCommand.java
+++ src/main/java/frc/robot/commands/AutoTripleMagazineCommand.java
@@ -4,8 +4,8 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+// import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+// import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.Magazine;
