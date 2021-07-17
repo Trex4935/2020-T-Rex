ea14ea8322199348782bdb9f5ba338b8d3249c78
Canthv0 <matbyrd@forthebyrds.com>
2021-07-17 10:58:22 -0400

    comment update

--- src/main/java/frc/robot/TrajectoryRepo.java
+++ src/main/java/frc/robot/TrajectoryRepo.java
@@ -4,18 +4,17 @@ import edu.wpi.first.wpilibj.trajectory.Trajectory;
 
 public class TrajectoryRepo {
 
-    // General Trajectory
-    public static final String trajectoryJSONSim = Constants.pathSim + "Slalom.wpilib.json";
-    public static final String trajectoryJSONRobot = Constants.pathRobot + "Back5_0.wpilib.json";
-    public static final Trajectory trajectory = TrajectoryContainer.makeTrajectory(trajectoryJSONRobot);
+        // General Trajectory
+        public static final String trajectoryJSONSim = Constants.pathSim + "Slalom.wpilib.json";
+        public static final String trajectoryJSONRobot = Constants.pathRobot + "Back5_0.wpilib.json";
+        public static final Trajectory trajectory = TrajectoryContainer.makeTrajectory(trajectoryJSONRobot);
 
-    // Autonomous Trajectory
+        // Autonomous Trajectory
+        // Build Trajectory objects to be consumed by auto commands
 
-    // Shooter Summer 
-    public static final Trajectory trajectoryFront = TrajectoryContainer
-    .makeTrajectory(Constants.pathRobot + "Back5_0.wpilib.json");  
+        public static final Trajectory trajectoryFront = TrajectoryContainer
+                        .makeTrajectory(Constants.pathRobot + "Back5_0.wpilib.json");
 
-    
-    public static final Trajectory trajectoryBack = TrajectoryContainer
-    .makeTrajectory(Constants.pathRobot + "Front5_0.wpilib.json");  
+        public static final Trajectory trajectoryBack = TrajectoryContainer
+                        .makeTrajectory(Constants.pathRobot + "Front5_0.wpilib.json");
 }
