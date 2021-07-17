a79f8ac191e66e646811436d4fc078923ecc30bb
Canthv0 <matbyrd@forthebyrds.com>
2021-07-17 10:47:30 -0400

    cleanup

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -9,7 +9,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Timer;
 
 public class Elevator extends SubsystemBase {
 
