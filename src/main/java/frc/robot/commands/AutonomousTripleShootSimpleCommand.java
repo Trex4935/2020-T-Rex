f8d327f73a9032d7752665be6af3bbf61e16d3eb
Samuel Narcisse <narcisse118@gmail.com>
2021-08-07 12:07:01 -0400

    added back path and magazine feeed

--- src/main/java/frc/robot/commands/AutonomousTripleShootSimpleCommand.java
+++ src/main/java/frc/robot/commands/AutonomousTripleShootSimpleCommand.java
@@ -25,7 +25,7 @@ public class AutonomousTripleShootSimpleCommand extends SequentialCommandGroup  
     //replace dt::getPose with driveTrain::getPosition to use simulated encoder and gyro data for simulation and debugging
     addCommands(
       new EmptyMagToShooterCommand(mag).alongWith(new ShootPIDCommand(shooter)).withTimeout(7),
-      new RamseteCommand(TrajectoryRepo.trajectoryFront, dt::getPose, new RamseteController(Constants.kRamseteB, Constants.kRamseteZeta), Constants.kDriveKinematics, dt::move, dt)
+      new RamseteCommand(TrajectoryRepo.trajectoryFront, dt::getPose, new RamseteController(Constants.kRamseteB, Constants.kRamseteZeta), Constants.kDriveKinematics, dt::move, dt).alongWith(new SingulateBallCommand(mag).withTimeout(7))
       //new RamseteCommand(TrajectoryRepo.trajectoryBack, dt::getPose, new RamseteController(Constants.kRamseteB, Constants.kRamseteZeta), Constants.kDriveKinematics, dt::move, dt)
       );
   }
