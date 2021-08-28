caafa9162a5d2dbd587c18097c4e689e8a068cf5
Kyle <Kyle@trex.com>
2021-08-28 12:42:31 -0400

    fancy simple auto path

--- src/main/java/frc/robot/TrajectoryRepo.java
+++ src/main/java/frc/robot/TrajectoryRepo.java
@@ -6,9 +6,9 @@ public class TrajectoryRepo {
 
         // General Trajectory
         public static final String trajectoryJSONSim = Constants.pathSim + "Slalom.wpilib.json";
-        public static final String trajectoryJSONRobot = Constants.pathRobot + "AutoBall2_test27path.wpilib.json";
-        public static final String trajectoryJSONRobotAlt = Constants.pathRobot + "Back5_0.wpilib.json";
-        public static final String trajectoryJSONRobotDefault = Constants.pathRobot + "Back5_0.wpilib.json";
+        public static final String trajectoryJSONRobot = Constants.pathRobot + "AutoBall2_test43path.wpilib.json";
+        //public static final String trajectoryJSONRobotAlt = Constants.pathRobot + "Back5_0.wpilib.json";
+        //public static final String trajectoryJSONRobotDefault = Constants.pathRobot + "Back5_0.wpilib.json";
         public static final Trajectory trajectory = TrajectoryContainer.makeTrajectory(trajectoryJSONRobot);
         public static final Trajectory trajectoryAlt = TrajectoryContainer.makeTrajectory(trajectoryJSONRobot);
         public static final Trajectory trajectoryDefault = TrajectoryContainer.makeTrajectory(trajectoryJSONRobot);
@@ -18,8 +18,8 @@ public class TrajectoryRepo {
         // Build Trajectory objects to be consumed by auto commands
 
         public static final Trajectory trajectoryFront = TrajectoryContainer
-                        .makeTrajectory(Constants.pathRobot + "AutoBall2_test27path.wpilib.json");
+                        .makeTrajectory(Constants.pathRobot + "AutoBall2_test43path.wpilib.json");
 
-        public static final Trajectory trajectoryBack = TrajectoryContainer
-                        .makeTrajectory(Constants.pathRobot + "Front5_0.wpilib.json");
+        //public static final Trajectory trajectoryBack = TrajectoryContainer
+                        //.makeTrajectory(Constants.pathRobot + "Front5_0.wpilib.json");
 }
