d406f218d0920b931ebeb2c42ab82a6ae376f5a8
Kyle <Kyle@trex.com>
2021-10-02 11:11:22 -0400

    small edits to comments and odomentary

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -179,7 +179,6 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    //m_robotContainer.driveTrain.initOdometry()
   }
 
   /** This function is called periodically during operator control. */
